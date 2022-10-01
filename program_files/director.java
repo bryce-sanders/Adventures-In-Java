6adf6683e25ee81fc5d62514631993e932e97d77
Bryce Sanders <104106284+bryce-sanders@users.noreply.github.com>
2022-09-30 20:22:37 -0700

    Developing Classes
    I have begun to create the class files for the program.

--- program_files/director.java
+++ program_files/director.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;  // Import the Scanner class for user input
+
 public class director {
+  final int tuition = 2200;
+
     public static void main(String[] args) {
-      System.out.println("Hello World");
+      // Create the String variable to hold the text for the main menu
+      String main_menu = "Welcome to Studentity."
+      + "\nPlease enter the number of the option you would like"
+      + "\nto select from the list bellow."
+      + "\n"
+      + "\n1. Enter New Student"
+      + "\n2. Search Existing Student"
+      + "\n3. Exit"
+      + "\n";
+
+      System.out.println(main_menu);  // Display the main menu
+      
+      Scanner in = new Scanner(System.in);  // Create a Scanner object
+      System.out.print("Enter Selection: ");
+      String selection = in.nextLine();  // Read user input
+      in.close();  // Close the Scanner
     }
   }
   
---
+++ program_files/semester.java
@@ -0,0 +1,20 @@
+public class semester {
+    int year = 0;
+    String season = "";
+
+    public static void get_year(String[] args) {
+        
+      }
+    
+    public static void set_year(String[] args) {
+        
+    }
+
+    public static void get_season(String[] args) {
+        
+    }
+
+    public static void set_season(String[] args) {
+        
+    }
+}
